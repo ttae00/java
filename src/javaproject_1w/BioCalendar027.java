531537a14ce8fdba47c6fe9603dcb6884d4e3df5
taeyoung <xodbd9756@naver.com>
2020-08-26 20:25:55 +0900

    second commit

--- src/javaproject_1w/BioCalendar027.java
+++ src/javaproject_1w/BioCalendar027.java
@@ -1,4 +1,4 @@
-package javaproject;
+package javaproject_1w;
 
 public class BioCalendar027 {
 	public static final int PHYSICAL = 23;	
@@ -9,6 +9,8 @@ public class BioCalendar027 {
 		// Math.PI 3.14에서 정의되어 있는 상수
 		double vals=(days % index) * 2 * Math.PI / index ;
 		System.out.println( Math.toDegrees(vals) +"도");
+		//2*PI*r 라디안을 각도로 환산
+		//toDegrees() : 각도변환
 	}
 
 }
